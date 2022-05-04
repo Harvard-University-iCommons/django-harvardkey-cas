f1b3a11bf00d82a033d9e29a4d8d091eb6eff12a
Patrick Tonne <patrick_tonne@harvard.edu>
2022-05-04 11:07:36 -0400

    bump version to 3.0

--- setup.py
+++ setup.py
@@ -9,7 +9,7 @@ os.chdir(os.path.normpath(os.path.join(os.path.abspath(__file__), os.pardir)))
 
 setup(
     name='django-harvardkey-cas',
-    version='2.4',
+    version='3.0',
     packages=find_packages(),
     include_package_data=True,
     license='BSD License',  # example license
